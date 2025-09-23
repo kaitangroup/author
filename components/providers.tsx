2fd791d8e651202909d3b3d1387e39b0b7d059e9
Sharif <sharif@Sharifs-MacBook-Air.local>
2025-09-23 20:21:24 +0600

    modified

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,13 +5,24 @@ import { Toaster } from "@/components/ui/sonner";
 import NextAuthSessionProvider from "@/components/providers/SessionProvider";
 import LoadingProvider from "@/components/LoadingOverlay/LoadingProvider";
 
+// export default function Providers({ children }: { children: React.ReactNode }) {
+//   return (
+//     <LoadingProvider minMs={2000}>
+//       <NextAuthSessionProvider>
+//         {children}
+//         <Toaster />
+//       </NextAuthSessionProvider>
+//     </LoadingProvider>
+//   );
+// }
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <LoadingProvider minMs={2000}>
+  
       <NextAuthSessionProvider>
         {children}
         <Toaster />
       </NextAuthSessionProvider>
-    </LoadingProvider>
+
   );
 }
