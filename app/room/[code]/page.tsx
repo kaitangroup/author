0395b9f00ea0c6816ff039ac1ec60ebe0e343c99
Sharif <sharif@Sharifs-MacBook-Air.local>
2026-03-25 19:31:28 +0600

    turn added

--- app/room/[code]/page.tsx
+++ app/room/[code]/page.tsx
@@ -16,7 +16,18 @@ type RoomPageProps = {
 };
 
 
-const ICE: RTCConfiguration = { iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }] };
+// const ICE: RTCConfiguration = { iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }] };
+
+const ICE: RTCConfiguration = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+  ]
+};
 
 export default function RoomPage({ params }: RoomPageProps) {
   const { data: session, status } = useSession();  
